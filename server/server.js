0767334ed8b39729be3ff03fc198919e25a8743b
Billy Tran <billy.tran61@gmail.com>
2019-08-24 10:08:13 -0400

    add basic route handler with no parsing middleware yet

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = 3000;
+
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../index.html'));
 });
@@ -9,6 +10,11 @@ app.get('/', (req, res) => {
 if (process.env.NODE_ENV === 'production') {
   app.use('/build', express.static(path.join(__dirname, '../build')));
 }
+
+// set up route to middleware that will handle parsing logic
+// parse the body of the request that will contain the string of code from the editor
+
+app.post('/run', (req, res) => {});
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}...`);
