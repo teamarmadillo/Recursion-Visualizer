883edfc56158737a726f5ffab5b1739cd13f6d84
Billy Tran <billy.tran61@gmail.com>
2019-08-24 11:00:55 -0400

    include body parser middleware for one route

--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
+const parserController = require('./controllers/parser');
 const PORT = 3000;
+
+// middleware for parsing body of request
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../index.html'));
@@ -14,7 +19,16 @@ if (process.env.NODE_ENV === 'production') {
 // set up route to middleware that will handle parsing logic
 // parse the body of the request that will contain the string of code from the editor
 
-app.post('/run', (req, res) => {});
+app.post(
+  '/run',
+  urlencodedParser,
+  parserController.convertToAST,
+  parserController.convertToRecursiveCallTree,
+  (req, res) => {
+    // deconstruct recursiveCallTree from res.locals
+    // send tree back to client for front-end to parse
+  }
+);
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}...`);
