4e4c23f45503917c12a3dea01b9c7be854820a14
Andy Wong <andywong.ny@gmail.com>
2019-08-26 17:00:29 -0400

    added to middleware (convertToAST, parseArgsAndBaseCases (No base case parse yet)

--- server/controllers/parser.js
+++ server/controllers/parser.js
@@ -1,14 +1,19 @@
 const parserController = {};
-// require espree
+const espree = require("espree");
 
 // use espree to convert to AST and save to res.locals.abstractSyntaxTree
 parserController.convertToAST = (req, res, next) => {
+  const ast = espree.parse(req.body);
+  res.locals.abstractSyntaxTree = ast;
   next();
 };
 
 // deconstruct ast from res.locals to use for algorithm
 // grab args from the AST object for the algorithm and save to res.locals.astArgsAndBaseCases
 parserController.parseArgsAndBaseCases = (req, res, next) => {
+  // TODO: Base cases
+  // Parsing Arguments in the AST
+  res.locals.astArgsAndBaseCases = res.locals.abstractSyntaxTree.body[1].expression.arguments[0].value;
   next();
 };
 
@@ -16,12 +21,14 @@ parserController.parseArgsAndBaseCases = (req, res, next) => {
 // create the State used in our react application
 // save to res.locals.state
 parserController.createState = (req, res, next) => {
+  // TODO: Will to add
   next();
 };
 
 // algorithm for creating the RCT
 // save resulting object of recursive calls to res.locals.recursiveCallTree
 parserController.convertToRecursiveCallsTree = (req, res, next) => {
+  // LOL
   next();
 };
 
