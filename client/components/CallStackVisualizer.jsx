a4e74d8ff32644a450ebad759bb5d1bdb3f1be4d
Billy Tran <billy.tran61@gmail.com>
2019-08-29 11:32:03 -0400

    render calls off mock data, next step to render off state

--- client/components/CallStackVisualizer.jsx
+++ client/components/CallStackVisualizer.jsx
@@ -3,10 +3,27 @@ import { Component } from 'react';
 
 class CallStackVisualizer extends Component {
   render() {
+    // render off mock data, fib(3);
+    const calls = [
+      { name: 'fib', parent: null, arg: 3, result: 2 },
+      { name: 'fib', parent: 3, arg: 2, result: 1 },
+      { name: 'fib', parent: 2, arg: 1, result: 1 },
+      { name: 'fib', parent: 2, arg: 0, result: 0 },
+      { name: 'fib', parent: 3, arg: 1, result: 1 }
+    ];
+    const callsArr = [];
+    for (let i = 0; i < calls.length; i += 1) {
+      callsArr.push(
+        <div className='callstack-frame'>
+          {`${calls[i].name}(${calls[i].arg})`}
+        </div>
+      );
+    }
+
     return (
       <div id='stack-container'>
-        <h3>Total Calls</h3>
-        <p className='callstack-frame'>fib(4)</p>
+        <h2>Total Calls</h2>
+        {callsArr}
       </div>
     );
   }
