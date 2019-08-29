325c4937234c19b3ac432eb81a1ce8d69fe871f1
William Magee <Will@Williams-MacBook-Pro.local>
2019-08-29 17:35:31 -0400

    almost fully implemented parse Args and basecases

--- server/controllers/parser.js
+++ server/controllers/parser.js
@@ -22,11 +22,9 @@ parserController.parseArgsAndBaseCases = (req, res, next) => {
   const funcName = AST.body[1].expression.callee.name;
   const funcArg = AST.body[1].expression.arguments[0].value;
 
-  const bc = AST.body[0].body.body[0].test.right.value;
   const bcArr = [];
-  for (let i = 0; i <= bc; i++) { bcArr.push(i); }
 
-  const funcCalls = AST.body[0].body.body[0].alternate
+  const funcCalls = AST.body[0].body.body[0]
   
   const func = new Function(`return(${res.locals.stringFunc})`);
 
@@ -56,5 +54,60 @@ parserController.convertToRecursiveCallsTree = (req, res, next) => {
   res.locals.config = ReactTreeConfig.NodeStructMaker(res.locals.recursiveStates);
   next();
 };
+
+const AST = flow.parse(['function fib2(n) {',
+  'if (n === 0) return 0',
+  'else if (n === 1) return 1;',
+  'else return fib(n - 1) + fib(n - 2) + fib(n - 3)}'].join('\n'));
+
+const bcArr = [];
+const funcCalls = AST.body[0].body.body[0];
+
+let counter = 0;
+function parsingAst(astObj) {
+  if (astObj.type === "IfStatement") {
+    parsingAst(astObj.test);
+    parsingAst(astObj.alternate);
+  } 
+  else if (astObj.right !== undefined) {
+    if (astObj.right.value !== undefined) {
+      console.log("HERE");
+      bcArr.push(astObj.right.value);
+    }
+    return;
+  }
+  else if (astObj.type === "ReturnStatement" || astObj.type === "BinaryExpression") {
+      if (astObj.argument.left.type === "CallExpression") {
+        console.log("BinaryLeft");
+        counter += 1;
+      }
+      else if (astObj.argument.left.type === "BinaryExpression") {
+        parsingAst(astObj.argument.left);
+      }
+
+      if (astObj.argument.right.type === "CallExpression") {
+        console.log("BinaryRight");
+        counter += 1;
+      }
+      else if (astObj.argument.right.type === "BinaryExpression") {
+        parsingAst(astObj.argument.right);
+      }
+      return;
+  }
+    
+  else if (astObj.argument.type === "CallExpression") {counter += 1; return;}
+  else if (astObj.left.type === "CallExpression" || astObj.right.type === "CallExpression") {counter += 1; return;}
+  // else if (astObj.right.type === "CallExpression")
+  return;
+}
+
+
+console.log(funcCalls.alternate.alternate.argument);
+parsingAst(funcCalls);
+console.log(counter);
+console.log(bcArr);
+
+
+
 
 module.exports = parserController;
